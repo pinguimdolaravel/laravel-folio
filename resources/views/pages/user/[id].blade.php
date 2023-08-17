a33abbe2e5165fc8bf6b63c6fee0442801d7258c
Rafael Lunardelli <rafael@devsquad.com>
2023-08-17 16:51:36 -0600

    wip

--- resources/views/pages/user/[id].blade.php
+++ resources/views/pages/user/[id].blade.php
@@ -1,6 +1,13 @@
+@php
+
+    $user = \App\Models\User::find($id);
+
+@endphp
+
 <x-layout.app>
     <div>
         <h1>user.id</h1>
         <h2>{{ $id }}</h2>
+        <h2>{{ $user->name }}</h2>
     </div>
 </x-layout.app>
--- resources/views/pages/user/index.blade.php
+++ resources/views/pages/user/index.blade.php
@@ -1,5 +1,19 @@
+@php
+
+    $users = \App\Models\User::all();
+
+@endphp
+
 <x-layout.app>
     <div>
         <h1>user.index</h1>
+
+        <ul>
+            @foreach($users as $user)
+
+                <li>{{ $user->name }}</li>
+
+            @endforeach
+        </ul>
     </div>
 </x-layout.app>
